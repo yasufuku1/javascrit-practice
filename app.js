2546defde56aebfc1d631a6e47b56c6a33166e95
yasusu_ 500 <eriyasu@gmail.com>
2023-02-01 18:01:52 +0900

    [Start]ヘッダーハンバーガメニュー作成

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert('Hello World');
+$(".openbtn").click(function(){
+    $(this).toggleClass('active');
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
